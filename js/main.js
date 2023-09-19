c2cc20d71ed694d295a8dfc1f5d5de442b858900
Silvia <silviatc1993@gmail.com>
2023-09-19 09:57:45 +0200

    added cpu random movement function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,34 @@
 'use strict';
 
 //Variables:
-const btnPlay = document.querySelector('.');
-const selectInput = document.querySelector('.');
-const result = document.querySelector('.');
-const playerCount = document.querySelector('.');
-const cpuCount = document.querySelector('.');
+const selectInput = document.querySelector('.js-input-select');
+const btnPlay = document.querySelector('.js-btn');
+const result = document.querySelector('.js-msg');
+const playerCount = document.querySelector('.js-player-count');
+const cpuCount = document.querySelector('.js-cpu-count');
 
 //Función para que la maquina haga una jugada aleatoria:
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
+}
+
+//Función para movimiento aleatorio de CPU:
+function randomCpuMove() {
+  let randomNum = getRandomNumber(9);
+  if (randomNum <= 3) {
+    return 'Piedra';
+  } else if (randomNum >= 7) {
+    return 'Papel';
+  } else {
+    return 'Tijera';
+  }
+}
+
+//Función para comparar jugadas entre el usuario y la CPU:
+function handleClick() {
+  const playerMove = selectInput.value;
+  const cpuMove = randomCpuMove();
+  let resultMEssage = '';
 }
 
 btnPlay.addEventListener('click', handleClick);
