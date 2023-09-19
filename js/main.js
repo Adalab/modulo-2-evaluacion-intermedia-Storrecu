c99a052741bf4130ef7860e0bdcfe593ea98bbf8
Silvia <silviatc1993@gmail.com>
2023-09-19 10:46:09 +0200

    functionality almost finished

--- js/main.js
+++ js/main.js
@@ -7,14 +7,13 @@ const result = document.querySelector('.js-msg');
 const playerCount = document.querySelector('.js-player-count');
 const cpuCount = document.querySelector('.js-cpu-count');
 let resultMessage = '';
-let Movements = 0;
 
 //Función para que la CPU haga una jugada aleatoria:
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-//Función para establecer resultado del movimiento aleatorio:
+//Función para establecer resultado del movimiento aleatorio de la CPU:
 function randomCpuMove() {
   let randomNum = getRandomNumber(9);
   if (randomNum <= 3) {
@@ -26,7 +25,26 @@ function randomCpuMove() {
   }
 }
 
-//Función para actualizar contador del jugador y de la CPU:
+//Función para comparar jugada del usuario y jugada CPU:
+function compareMovements() {
+  const playerMove = selectInput.value;
+  const cpuMove = randomCpuMove();
+
+  if (playerMove === cpuMove) {
+    resultMessage = 'Empate';
+  } else if (
+    (playerMove === 'Piedra' && cpuMove === 'Tijera') ||
+    (playerMove === 'Papel' && cpuMove === 'Piedra') ||
+    (playerMove === 'Tijera' && cpuMove === 'Papel')
+  ) {
+    resultMessage = '¡Has ganado!';
+  } else {
+    resultMessage = '¡Has perdido!';
+  }
+  result.innerHTML = resultMessage;
+}
+
+//Función para actualizar contador del jugador y de la CPU y que no se pongan números negativos:
 const countGame = () => {
   if (resultMessage === '¡Has ganado!') {
     playerCount.innerHTML++;
@@ -41,30 +59,22 @@ const countGame = () => {
   }
 };
 
-//Función para comparar jugadas entre el usuario y la CPU:
-function handleClick() {
-  const playerMove = selectInput.value;
-  const cpuMove = randomCpuMove();
+//Función para actualizar los movimientos del juego:
+const finalMovements = () => {
+  let Movements = 0;
+
   if (Movements >= 10) {
     result.innerHTML = '¡El juego ha terminado!';
+    Movements++;
   }
+};
 
-  if (playerMove === cpuMove) {
-    resultMessage = 'Empate';
-  } else if (
-    (playerMove === 'Piedra' && cpuMove === 'Tijera') ||
-    (playerMove === 'Papel' && cpuMove === 'Piedra') ||
-    (playerMove === 'Tijera' && cpuMove === 'Papel')
-  ) {
-    resultMessage = '¡Has ganado!';
-  } else {
-    resultMessage = '¡Has perdido!';
-  }
-  result.innerHTML = resultMessage;
-
+//Función manejadora al hacer click en el botón Jugar:
+const handleClick = () => {
+  compareMovements();
   countGame();
-  console.log(Movements++);
-}
+  finalMovements();
+};
 
 //Eventos:
 btnPlay.addEventListener('click', handleClick);
