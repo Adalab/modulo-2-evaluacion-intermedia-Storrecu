526dcc8c53020c4b3c5c11d21c873b1e7a1e919e
Silvia <silviatc1993@gmail.com>
2023-09-19 10:30:46 +0200

    almost completed handle function

--- js/main.js
+++ js/main.js
@@ -6,13 +6,15 @@ const btnPlay = document.querySelector('.js-btn');
 const result = document.querySelector('.js-msg');
 const playerCount = document.querySelector('.js-player-count');
 const cpuCount = document.querySelector('.js-cpu-count');
+let resultMessage = '';
+let Movements = 0;
 
-//Función para que la maquina haga una jugada aleatoria:
+//Función para que la CPU haga una jugada aleatoria:
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-//Función para movimiento aleatorio de CPU:
+//Función para establecer resultado del movimiento aleatorio:
 function randomCpuMove() {
   let randomNum = getRandomNumber(9);
   if (randomNum <= 3) {
@@ -24,11 +26,45 @@ function randomCpuMove() {
   }
 }
 
+//Función para actualizar contador del jugador y de la CPU:
+const countGame = () => {
+  if (resultMessage === '¡Has ganado!') {
+    playerCount.innerHTML++;
+    if (cpuCount.innerHTML > 0) {
+      cpuCount.innerHTML--;
+    }
+  } else if (resultMessage === '¡Has perdido!') {
+    cpuCount.innerHTML++;
+    if (playerCount.innerHTML > 0) {
+      playerCount.innerHTML--;
+    }
+  }
+};
+
 //Función para comparar jugadas entre el usuario y la CPU:
 function handleClick() {
   const playerMove = selectInput.value;
   const cpuMove = randomCpuMove();
-  let resultMEssage = '';
+  if (Movements >= 10) {
+    result.innerHTML = '¡El juego ha terminado!';
+  }
+
+  if (playerMove === cpuMove) {
+    resultMessage = 'Empate';
+  } else if (
+    (playerMove === 'Piedra' && cpuMove === 'Tijera') ||
+    (playerMove === 'Papel' && cpuMove === 'Piedra') ||
+    (playerMove === 'Tijera' && cpuMove === 'Papel')
+  ) {
+    resultMessage = '¡Has ganado!';
+  } else {
+    resultMessage = '¡Has perdido!';
+  }
+  result.innerHTML = resultMessage;
+
+  countGame();
+  console.log(Movements++);
 }
 
+//Eventos:
 btnPlay.addEventListener('click', handleClick);
